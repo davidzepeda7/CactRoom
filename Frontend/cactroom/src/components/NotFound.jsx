2dc7776b25188804f4370a63fed904a88bd3b2cd
David Zepeda <20230231@ricaldone.edu.sv>
2025-08-23 16:10:36 -0600

    Cambios menores

--- Frontend/cactroom/src/components/NotFound.jsx
+++ Frontend/cactroom/src/components/NotFound.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../styles/NotFound.css";
+import "../Styles/NotFound.css";
 
 const Error404 = () => {
   const navigate = useNavigate();
--- Frontend/cactroom/src/pages/Login.jsx
+++ Frontend/cactroom/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import "../styles/Login.css";
+import "../Styles/Login.css";
 
 function Login() {
   const [email, setEmail] = useState("");
